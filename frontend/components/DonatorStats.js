1aee433d0b29cfd5874a1a70b21d45041fed0d53
shan5742 <asamshan456@gmail.com>
2022-04-30 10:16:22 +0100

    adding scrollable rankings

--- frontend/components/DonatorStats.js
+++ frontend/components/DonatorStats.js
@@ -8,19 +8,21 @@ export const DonatorStats = ({ donatorList }) => {
   return (
     <div className={s.container}>
       <h1>Top Donators 🔥</h1>
-      {sortedArray.map((donator) => {
-        const amount = ethers.utils.formatEther(donator.amountDonated);
-        return (
-          <div key={donator.walletAddress} className={s.donator}>
-            <img
-              src={`https://web3-images-api.kibalabs.com/v1/accounts/${donator.walletAddress}/image`}
-              className={s.avatar}
-            />
-            <p className={s.address}>{donator.walletAddress}</p>
-            <p className={s.amount}>{amount} ETH</p>
-          </div>
-        );
-      })}
+      <div className={s.wrapper}>
+        {sortedArray.map((donator) => {
+          const amount = ethers.utils.formatEther(donator.amountDonated);
+          return (
+            <div key={donator.walletAddress} className={s.donator}>
+              <img
+                src={`https://web3-images-api.kibalabs.com/v1/accounts/${donator.walletAddress}/image`}
+                className={s.avatar}
+              />
+              <p className={s.address}>{donator.walletAddress}</p>
+              <p className={s.amount}>{amount} ETH</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
